fa607a11bddce50b02576cdc822455ce8487d41d
rens4000 <mulderrens@outlook.com>
2019-02-21 08:54:11 +0100

    RELEASE VERSION 1.1

--- Music Sorter/src/nl/rensmulder/musicsorter/MusicSorter.java
+++ Music Sorter/src/nl/rensmulder/musicsorter/MusicSorter.java
@@ -12,20 +12,82 @@ import java.util.Scanner;
 public class MusicSorter {
 	
 	private Map<Integer, File> files = new LinkedHashMap<Integer, File>();
-	
 	private String[] allowedExtensions = {"mp3", "wav", "aac", "ogg", "wma"};
+	
 	
 	public MusicSorter() {
 		Scanner reader = new Scanner(System.in);
-		String path = retrievePath(reader);
 		
-		putFilesInArray(path);
-		renameFiles();
-		
-		System.out.println("==============================================");
-		System.out.println("All the files have been renamed. Have fun!");
+		start(reader);
 	}
 	
+	/*
+	 * First method that is executed by the program.
+	 * Note: Method also gets recalled when the program is done with tasks.
+	 */
+	public void start(Scanner reader) {
+		String command = getCommand(reader);
+		executeCommand(command, reader);
+	}
+	
+	/*
+	 * Executes the given command.
+	 * Note: It does not have a method to go when the command does not
+	 * exist. This is because the existence of the command gets checked in
+	 * getCommand();
+	 */
+	private void executeCommand(String command, Scanner reader) {
+		if(command.equalsIgnoreCase("undo")) {
+			String path = retrievePath(reader);
+			
+			undoFiles(path);
+			
+			System.out.println("==============================================");
+			System.out.println("All the files have been unnumbered.");
+			
+		} else if(command.equalsIgnoreCase("rename")) {
+			String path = retrievePath(reader);
+			
+			putFilesInArray(path);
+			renameFiles();
+			
+			System.out.println("==============================================");
+			System.out.println("All the files have been renamed. Have fun!");
+		}
+		start(reader);
+	}
+
+	/*
+	 * "Preparation" method for retrieving the command.
+	 */
+	private String getCommand(Scanner reader) {
+		
+		String command;
+		
+		System.out.println("Please specify the command you want to use(rename/undo): ");
+		
+		command = retrieveCommand(reader);
+		
+		return command;
+	}
+	
+	/*
+	 * Method that retrieves the command. Gets called by getCommand();
+	 */
+	private String retrieveCommand(Scanner reader) {
+		
+		String command = reader.nextLine();
+		
+		if(!command.equalsIgnoreCase("undo") && !command.equalsIgnoreCase("rename")) {
+			System.out.println("Command not found.");
+			System.out.println("Please specify the command you want to use(rename/undo): ");
+			command = retrieveCommand(reader);
+		}
+		
+		return command;
+		
+	}
+
 	private String retrievePath(Scanner reader) {
 		
 		System.out.println("Path of your music library: ");
@@ -55,6 +117,38 @@ public class MusicSorter {
 		
 		for(int i = 0; i < files.size(); i++) {
 			renameFile(localFiles.get(i), ids.get(i));
+		}
+		
+	}
+	
+	private void undoFiles(String path) {
+		File[] pathFiles = new File(path).listFiles();
+		
+		for(File file : pathFiles) {
+			undoFile(file);
+		}
+		
+	}
+	
+	private void undoFile(File file) {
+		if(!isMusicFile(file)) {
+			System.out.println("[warning] " + file.getName() + " is not an audio file, ignoring!");
+			return;
+		}
+		
+		if(!fileHasBeenNumbered(file)) {
+			System.out.println("[info] " + file.getName() + " is not numbered, ignoring!");
+			return;
+		}
+		
+		String path = file.getParent();
+		
+		String name = file.getName().substring(4, file.getName().length());
+		
+		File newFile = new File(path + "\\" + name);
+		
+		if(!file.renameTo(newFile)) {
+			System.out.println("An error occured while renaming: " + name);
 		}
 		
 	}
@@ -118,6 +212,9 @@ public class MusicSorter {
 		return new File(path).exists();
 	}
 	
+	/*
+	 * Checks if the file has a music file extension.
+	 */
 	private boolean isMusicFile(File file) {
 		String extension = "";
 
